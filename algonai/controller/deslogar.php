5f6a83db0abbef627d6e10c3b36c201556d9c18a
Pedro Tosta <114268443+PedroTosta@users.noreply.github.com>
2025-02-20 17:06:07 -0400

    Delete algonai directory

--- algonai/controller/deslogar.php
+++
@@ -1,6 +0,0 @@
-<?php ob_start(); ?>
-<?php
-    if (!isset($_SESSION)) session_start();
-    session_destroy(); // Destrói a sessão limpando todos os valores salvos
-    header("Location: ../view/login.php"); exit; // Redireciona o visitante
-  ?>
